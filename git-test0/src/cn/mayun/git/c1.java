4ba480dc7a830211cca9861cb98adcd428592732
my <mayun1008@163.com>
2020-06-23 13:33:34 +0800

    tianjia

--- git-test0/src/cn/mayun/git/c1.java
+++ git-test0/src/cn/mayun/git/c1.java
@@ -2,7 +2,7 @@ package cn.mayun.git;
 
 public class c1 {
     public static void main(String[] args) {
-        System.out.println(int i=0;);
+        System.out.println(0);
     }
     int i = 0;
 }
